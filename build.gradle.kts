4c46541866df14fa716ab522090bbe7f0a88e722
rafa <pcihazer20@gmail.com>
2026-03-04 10:53:39 -0700

    add spring boot actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     // Spring Boot starters
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     // PostgreSQL driver
